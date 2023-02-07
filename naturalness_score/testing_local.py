0570a1f68be0895f4146e98118e4ef9ca711599f
JabalnurIT <jabalnur.it@gmail.com>
2023-02-07 21:50:39 +0800

    add model

--- naturalness_score/testing_local.py
+++ naturalness_score/testing_local.py
@@ -18,7 +18,7 @@ print('Execution time :', et1-st1, 'seconds')
 print('Avarage Perplexity')
 print('Execution time:', et2-st2, 'seconds')
 
-print(9831050005000007%6)
+# print(9831050005000007%6)
 
 # Run In pipenv terminal 
 # python naturalness_score/testing_local.py
